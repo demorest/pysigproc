f75c9fb53774f8e08ba66ffb46158a33b1a6d723
Paul Demorest <pdemores@nrao.edu>
2016-04-28 19:44:34 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysigproc',
+      version='0.0',
+      description='Python reader/writer for sigproc filterbank files',
+      author='Paul Demorest',
+      author_email='pdemores@nrao.edu',
+      url='http://github.com/demorest/pysigproc',
+      py_modules=['pysigproc']
+     )
