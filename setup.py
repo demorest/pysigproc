0fd41b14dffa83ce9cc211f7c0752991a1d0318a
devanshkv <devansh.kv@gmail.com>
2018-06-21 13:27:34 -0400

    fixed syntax error

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 setup(name='pysigproc',
       version='0.1',
       description='Python reader/writer for sigproc filterbank files (works with python3 as well)',
-      author='Paul Demorest, Devansh Agarwal'
+      author='Paul Demorest, Devansh Agarwal',
       author_email='pdemores@nrao.edu, da0017@mix.wvu.edu'
-      url='http://github.com/demorest/pysigproc',
+      url='http://github.com/devanshkv/pysigproc',
       py_modules=['pysigproc']
      )
