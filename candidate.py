b9192bed2b4650bd5bc77d6d848e33906e5ebc35
Kshitij Aggarwal <kshitij@bowser.phys.wvu.edu>
2018-08-31 23:33:49 -0400

    added save to hdf5 to candidate

--- candidate.py
+++ candidate.py
@@ -6,11 +6,43 @@ from pysigproc import SigprocFile
 from scipy.optimize import golden
 
 class Candidate(SigprocFile):
-    def __init__(self,fp=None,dm=None,tcand=0,width=0):
+    def __init__(self,fp=None,dm=None,tcand=0,width=0,label=np.nan,snr=None):
         SigprocFile.__init__(self, fp)
         self.dm=dm
         self.tcand=tcand
         self.width=width
+        self.label=label
+        self.snr=snr
+
+    def save_h5(self,fnout=None):
+        if fnout is None:
+            fnout=f'cand_dm_{self.dm}_snr_{self.snr}_time_{self.tcand}.h5'
+        with  h5py.File(fnout, 'w') as f:
+            freq_time_dset = f.create_dataset('data_freq_time', data=self.dedispersed)
+            freq_time_dset.dims[0].label = b"time"
+            freq_time_dset.dims[1].label = b"frequency"
+
+            labels_dset =  f.create_dataset('labels', data=[self.label])
+            labels_dset.dims[0].label = b"labels"         
+
+            if self.dmt is not None:
+                dm_time_dset = f.create_dataset('data_dm_time', data=self.dmt)
+                dm_time_dset.dims[0].label = b"dm"
+                dm_time_dset.dims[1].label = b"time"
+            #if params is not None:
+            #    f.create_dataset('params', data=params)
+            #if self.snr is not None:
+            #    snr_dset = f.create_dataset('snr', data=self.snr)
+            #    snr_dset.dim[0].label = b"snr"
+                
+            # Copy over header information as attributes
+            #for key in self._type.keys():
+            #    print(getattr(self, key))
+            #    if not getattr(self, key): 
+            #        freq_time_dset.attrs[key] = getattr(self, key)
+
+
+        return fnout
     
     def dispersion_delay(self,dms=None):
         if dms is None:
@@ -47,7 +79,7 @@ class Candidate(SigprocFile):
                 dedisp_arr[:,ii]=np.roll(self.data[:,ii],delay)
             self.dedispersed = dedisp_arr
         else:
-            self.dedipersed = None
+            self.dedispersed = None
         return self
 
     def dmtime(self):
@@ -56,9 +88,10 @@ class Candidate(SigprocFile):
         dmt=np.zeros((100,self.data.shape[0]))
         for ii,dm in enumerate(dm_list):
             dmt[ii,:]=self.dedisperse(dms=dm).dedispersed.sum(1)
-        return dmt
+        self.dmt=dmt
+        return self
 
-    def snr(self,time_series=None):
+    def get_snr(self,time_series=None):
         if time_series is None and self.dedispersed is None:
             return None
         if time_series is None:
@@ -77,9 +110,11 @@ class Candidate(SigprocFile):
             return None
         def dm2snr(dm):
             time_series=self.dedisperse(dm).dedispersed.sum(1)
-            return -self.snr(time_series)
+            return -self.get_snr(time_series)
         try:
-            out=golden(dm2snr,full_output=1,brack=(0,self.dm,2*self.dm),tol=1e-3)
+            out=golden(dm2snr,full_output=1,brack=(-self.dm/2,self.dm,2*self.dm),tol=1e-3)
         except ValueError:
             out=golden(dm2snr,full_output=1,tol=1e-3)
         return out[0],-out[1]
+
+    
