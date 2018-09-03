b14b496fdd33c422be812fcfcddf4c2d64e21f4c
devanshkv <devansh.kv@gmail.com>
2018-09-03 18:07:45 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
 
 setup(name='pysigproc',
       version='0.2',
       description='Python reader/writer for sigproc filterbank files (works with python3 as well)',
-      author='Paul Demorest, Devansh Agarwal', 'Kshitij Aggarwal'
-      author_email='pdemores@nrao.edu, da0017@mix.wvu.edu', 'ka0064@mix.wvu.edu'
+      author='Paul Demorest, Devansh Agarwal, Kshitij Aggarwal',
+      author_email='pdemores@nrao.edu, da0017@mix.wvu.edu, ka0064@mix.wvu.edu',
       url='http://github.com/devanshkv/pysigproc',
-      py_modules=['pysigproc,candidate']
+      packages=find_packages(),
+      py_modules=['pysigproc','candidate']
      )
