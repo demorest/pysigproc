84d6f3d73e872d7e8989cf030d31cf1b0cb52169
devanshkv <devansh.kv@gmail.com>
2018-09-24 15:38:07 -0400

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 setup(name='pysigproc',
-      version='0.25',
+      version='0.3,
       description='Python reader/writer for sigproc filterbank files (works with python3 as well)',
       author='Paul Demorest, Devansh Agarwal, Kshitij Aggarwal',
       author_email='pdemores@nrao.edu, da0017@mix.wvu.edu, ka0064@mix.wvu.edu',
