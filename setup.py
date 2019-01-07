8ce7be9b1fae91a86cbe9f8fb57fce00e56a3dc7
Devansh Agarwal <devanshkv@gmail.com>
2019-01-07 14:41:39 -0500

    h5plotter optional detrend

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 setup(name='pysigproc',
-      version='0.35',
+      version='0.36',
       description='Python reader/writer for sigproc filterbank files (works with python3 as well)',
       author='Paul Demorest, Devansh Agarwal, Kshitij Aggarwal',
       author_email='pdemores@nrao.edu, da0017@mix.wvu.edu, ka0064@mix.wvu.edu',
       url='http://github.com/devanshkv/pysigproc',
       packages=find_packages(),
-      py_modules=['pysigproc','candidate']
+      py_modules=['pysigproc','candidate','h5plotter']
      )
