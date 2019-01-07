f752094842029d53eb54a1c6a777eeb6bf942916
Devansh Agarwal <devanshkv@gmail.com>
2019-01-07 14:46:30 -0500

    fixed detrend error

---
+++ h5plotter.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import matplotlib
+matplotlib.use('TkAgg')
+import h5py
+import sys
+import pylab as plt
+import os
+import numpy as np
+import glob
+import argparse
+from tqdm import *
+from multiprocessing import Pool
+import scipy.signal as s
+os.environ['HDF5_USE_FILE_LOCKING']='FALSE'
+from matplotlib import gridspec
+
+def plot_h5(h5_file,show=False, save=True, detrend=True):
+        plt.clf()
+        fig=plt.figure(figsize=(15,8))
+        with h5py.File(h5_file,'r') as f:
+            to_print=[]
+            for key in f.attrs.keys():
+                to_print.append(f'{key} : {f.attrs[key]}\n')
+            str_print = ''.join(to_print)
+            dm_time=np.array(f['data_dm_time'])
+            if detrend:
+                freq_time=s.detrend(np.array(f['data_freq_time'])[:,::-1].T)
+            else:
+                freq_time=np.array(f['data_freq_time'])[:,::-1].T
+            dm_time[dm_time!=dm_time]=0
+            freq_time[freq_time!=freq_time]=0
+            freq_time-=np.median(freq_time)
+            freq_time/=np.std(freq_time)
+            nt,nf=freq_time.shape
+            gs = gridspec.GridSpec(3,2,width_ratios=[4, 1], height_ratios=[1,1,1])#,figsize=(8,6),sharex=True)
+            ax1 = plt.subplot(gs[0, 0])
+            ax2 = plt.subplot(gs[1,0])
+            ax3 = plt.subplot(gs[2, 0])
+            ax4 = plt.subplot(gs[:, 1])
+            fch1,foff,nchan,dm,cand_id,tsamp,dm_opt,snr,snr_opt,width=f.attrs['fch1'],\
+                    f.attrs['foff'],f.attrs['nchans'],f.attrs['dm'],f.attrs['cand_id'],\
+                    f.attrs['tsamp'],f.attrs['dm_opt'],f.attrs['snr'],f.attrs['snr_opt'],f.attrs['width']
+            if width > 1:
+                ts=np.arange(freq_time.shape[0])*tsamp*np.min([1,width//2])
+            else:
+                ts=np.arange(freq_time.shape[0])*tsamp*np.min([1,width])
+            ax1.plot(ts,freq_time.mean(0),'k-')
+            ax1.set_ylabel('Flux (Arb. Units)')
+            ax2.imshow(freq_time, aspect='auto',extent=[ts[0],ts[-1],fch1,fch1+(nchan*foff)],interpolation='none')
+            ax2.set_ylabel('Frequency (MHz)')
+            ax3.imshow(dm_time,aspect='auto',extent=[ts[0],ts[-1],2*dm,0],interpolation='none')
+            ax3.set_ylabel(r'DM (pc cm$^{-3}$)')
+            ax3.set_xlabel('Time (s)')
+            ax4.text(-0.6, 0, str_print, fontsize=14,ha='left', va='bottom',  wrap=True)
+            ax4.axis('off')
+            plt.tight_layout()
+            if save:
+                plt.savefig(h5_file[:-3]+'.png',bbox_inches='tight')
+            if show:
+                plt.show()
+            else:
+                plt.close()
+
+
+if __name__ == '__main__':
+    with Pool(processes=4) as p:
+        args=glob.glob(sys.argv[1])
+        max_ = len(args)
+        with tqdm(total=max_) as pbar:
+            for i, _ in tqdm(enumerate(p.imap_unordered(plot_h5, args,chunksize=2))):
+                pbar.update()
